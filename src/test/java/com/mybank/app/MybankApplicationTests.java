696657da1f3976ec454fe16c988eba8a766ca4ce
Ramveer Singh <rsverma1193@hotmail.com>
2021-01-24 19:07:45 +0530

    First Commit

---
+++ src/test/java/com/mybank/app/MybankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybank.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
